893b6c6ede707052ed24a9978e33257d426a20bf
Olivier Richard <olivier.richard@imag.fr>
2019-12-09 20:26:33 +0100

    Add some services

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,8 @@
 {
-  # Add your NixOS modules here
-  #
-  # my-module = ./my-module;
+  batsky = ./services/batsky.nix;
+  bs-slurm = ./services/bs-slurm.nix;
+  fe-slurm = ./services/fe-slurm.nix;
+  rs-munge = ./services/bs-munge.nix;
+  oar = ./services/oar.nix;
 }
 
