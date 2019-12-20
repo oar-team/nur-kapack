c57e456cd277f373403ae91f8ed9a1f5a4bc41d7
Olivier Richard <olivier.richard@imag.fr>
2019-12-20 18:18:37 +0100

    [nur-ci] does not support pkgs.replaceDependency in default.nix

--- default.nix
+++ default.nix
@@ -14,53 +14,53 @@ rec {
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  # glibc-batsky = pkgs.glibc.overrideAttrs (attrs: {
-  #   patches = attrs.patches ++ [ ./pkgs/glibc-batsky/clock_gettime.patch
-  #     ./pkgs/glibc-batsky/gettimeofday.patch ];
-  #   postConfigure = ''
-  #     export NIX_CFLAGS_LINK=
-  #     export NIX_LDFLAGS_BEFORE=
-  #     export NIX_DONT_SET_RPATH=1
-  #     unset CFLAGS
-  #     makeFlagsArray+=("bindir=$bin/bin" "sbindir=$bin/sbin" "rootsbindir=$bin/sbin" "--quiet")
-  #   '';
-  # });
+  glibc-batsky = pkgs.glibc.overrideAttrs (attrs: {
+    patches = attrs.patches ++ [ ./pkgs/glibc-batsky/clock_gettime.patch
+      ./pkgs/glibc-batsky/gettimeofday.patch ];
+    postConfigure = ''
+      export NIX_CFLAGS_LINK=
+      export NIX_LDFLAGS_BEFORE=
+      export NIX_DONT_SET_RPATH=1
+      unset CFLAGS
+      makeFlagsArray+=("bindir=$bin/bin" "sbindir=$bin/sbin" "rootsbindir=$bin/sbin" "--quiet")
+    '';
+  });
 
-  # batsky = pkgs.callPackage ./pkgs/batsky { };
+  batsky = pkgs.callPackage ./pkgs/batsky { };
 
-  # procset = pkgs.callPackage ./pkgs/procset { };
+  procset = pkgs.callPackage ./pkgs/procset { };
   
-  # pybatsim = pkgs.callPackage ./pkgs/pybatsim { inherit procset; };
+  pybatsim = pkgs.callPackage ./pkgs/pybatsim { inherit procset; };
 
-  # pytest_flask = pkgs.callPackage ./pkgs/pytest-flask { };
+  pytest_flask = pkgs.callPackage ./pkgs/pytest-flask { };
 
-  # remote_pdb = pkgs.callPackage ./pkgs/remote-pdb { };
+  remote_pdb = pkgs.callPackage ./pkgs/remote-pdb { };
   
-  # oar = pkgs.callPackage ./pkgs/oar { inherit procset sqlalchemy_utils pytest_flask pybatsim remote_pdb; };
+  oar = pkgs.callPackage ./pkgs/oar { inherit procset sqlalchemy_utils pytest_flask pybatsim remote_pdb; };
 
-  # sqlalchemy_utils = pkgs.callPackage ./pkgs/sqlalchemy-utils { };
+  sqlalchemy_utils = pkgs.callPackage ./pkgs/sqlalchemy-utils { };
 
-  # # Setting needed for nixos-19.03 and nixos-19.09
-  # slurm-bsc-simulator =
-  #   if pkgs ? libmysql
-  #   then pkgs.callPackage ./pkgs/slurm-simulator { libmysqlclient = pkgs.libmysql; }
-  #   else pkgs.callPackage ./pkgs/slurm-simulator { };
-  # slurm-bsc-simulator-v17 = slurm-bsc-simulator;
+  # Setting needed for nixos-19.03 and nixos-19.09
+  slurm-bsc-simulator =
+    if pkgs ? libmysql
+    then pkgs.callPackage ./pkgs/slurm-simulator { libmysqlclient = pkgs.libmysql; }
+    else pkgs.callPackage ./pkgs/slurm-simulator { };
+  slurm-bsc-simulator-v17 = slurm-bsc-simulator;
   
-  # #slurm-bsc-simulator-v14 = slurm-bsc-simulator.override { version="14"; };
+  #slurm-bsc-simulator-v14 = slurm-bsc-simulator.override { version="14"; };
   
-  # slurm-multiple-slurmd = pkgs.slurm.overrideAttrs (oldAttrs: {
-  #   configureFlags = oldAttrs.configureFlags ++ ["--enable-multiple-slurmd" "--enable-silent-rules"];});
+  slurm-multiple-slurmd = pkgs.slurm.overrideAttrs (oldAttrs: {
+    configureFlags = oldAttrs.configureFlags ++ ["--enable-multiple-slurmd" "--enable-silent-rules"];});
   
-  # slurm-front-end = pkgs.slurm.overrideAttrs (oldAttrs: {
-  #   configureFlags = [
-  #     "--enable-front-end"
-  #     "--with-lz4=${pkgs.lz4.dev}"
-  #     "--with-zlib=${pkgs.zlib}"
-  #     "--sysconfdir=/etc/slurm"
-  #     "--enable-silent-rules"
-  #   ];
-  # });
+  slurm-front-end = pkgs.slurm.overrideAttrs (oldAttrs: {
+    configureFlags = [
+      "--enable-front-end"
+      "--with-lz4=${pkgs.lz4.dev}"
+      "--with-zlib=${pkgs.zlib}"
+      "--sysconfdir=/etc/slurm"
+      "--enable-silent-rules"
+    ];
+  });
 
   # bs-slurm = pkgs.replaceDependency {
   #   drv = slurm-multiple-slurmd;
