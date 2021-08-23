f4b3a3d2e7acca62a63aca785cb37a7f7d6734c0
Millian Poquet <millian.poquet@inria.fr>
2021-08-23 14:32:40 +0200

    batprotocol-cpp: init at ac766db

--- nur.nix
+++ nur.nix
@@ -51,6 +51,8 @@ rec {
 
   batexpe = pkgs.callPackage ./pkgs/batexpe { };
 
+  batprotocol-cpp = pkgs.callPackage ./pkgs/batprotocol/cpp.nix { inherit flatbuffers debug; };
+
   batsim-310 = pkgs.callPackage ./pkgs/batsim/batsim310.nix { inherit intervalset redox debug; simgrid = simgrid-324; };
   batsim-400 = pkgs.callPackage ./pkgs/batsim/batsim400.nix { inherit intervalset redox debug; simgrid = simgrid-325light; };
   batsim = batsim-400;
