8c2f3d907db117229fa11c95c9342f9026daf9c2
Millian Poquet <millian.poquet@inria.fr>
2021-11-10 15:29:22 +0100

    oar: mark as broken
    - CI ERROR: Could not find a version that satisfies the requirement fastapi>0.68.0 (from oar)
    - CI log: https://github.com/oar-team/nur-kapack/runs/4152808065?check_suite_focus=true#step:5:137

--- pkgs/oar/default.nix
+++ pkgs/oar/default.nix
@@ -49,7 +49,7 @@ python3Packages.buildPythonPackage rec {
   '';
 
   meta = {
-    #broken = true;
+    broken = true;
     homepage = "https://github.com/oar-team/oar3";
     description = "The OAR Resources and Tasks Management System";
     license = lib.licenses.lgpl3;
