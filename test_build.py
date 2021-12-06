92e1054421a712d2e5d3195118a08d6dbe4ba681
Millian Poquet <millian.poquet@inria.fr>
2021-12-06 13:37:00 +0100

    test: process execution verbosity++

--- test_build.py
+++ test_build.py
@@ -7,14 +7,16 @@ import pytest
 import shlex
 import subprocess
 
-def run_cmd_handle_failure(cmd, input=None):
+def run_cmd_handle_failure(cmd, input=None, do_print=False):
     p = subprocess.Popen(shlex.split(cmd), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
     out, err = p.communicate(input=input)
+    if do_print:
+        print(f"cmd: {cmd}")
+        #if input: print(f"stdin:\n--- BEGIN OF STDIN LOG ---\n{input}\n--- END OF STDIN LOG---")
+        if out: print(f"stdout:\n--- BEGIN OF STDOUT LOG ---\n{out}\n--- END OF STDOUT LOG---")
+        if err: print(f"stderr:\n--- BEGIN OF STDERR LOG ---\n{err}\n--- END OF STDERR LOG---")
     if p.returncode != 0:
         print(f'process failed: {cmd}')
-        if input: print(f"stdin:\n--- BEGIN OF STDIN LOG ---\n{input}\n--- END OF STDIN LOG---")
-        if out: print(f"stdout: {out}")
-        if err: print(f"stderr: {err}")
         raise Exception(f'Process failed (returned {p.returncode})')
     return p
 
@@ -30,7 +32,7 @@ def test_build(request, attribute, graph):
         run_cmd_handle_failure(f"cachix push {cachix_name}", input=input_paths)
 
     # build the package
-    run_cmd_handle_failure(f"nix-build -A {attribute} --arg debug {debug_str}")
+    run_cmd_handle_failure(f"nix-build -A {attribute} --arg debug {debug_str}", do_print=True)
 
     # push the package to cachix
     if request.config.getoption("--push-on-cachix"):
