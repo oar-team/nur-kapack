000e998c9dd8e7abeecfb814db14469c8766d25a
Olivier Richard <olivier.richard@imag.fr>
2022-01-30 18:18:48 +0100

    [oar] add support new api (fastapi) through nginx-unit

--- pkgs/oar/default.nix
+++ pkgs/oar/default.nix
@@ -1,44 +1,40 @@
-{ lib, pkgs, fetchFromGitHub, python3Packages, poetry, zeromq, procset, pybatsim, remote_pdb}:
+{ lib, pkgs, fetchFromGitHub, python3Packages, poetry, zeromq, procset, pybatsim, remote_pdb }:
 
 python3Packages.buildPythonPackage rec {
   name = "oar-${version}";
   version = "3.0.0.dev4";
   format = "pyproject";
-  #disabled = pythonOlder "3.8";
-  
+
   src = fetchFromGitHub {
-        owner = "oar-team";
-        repo = "oar3";
-        rev = "031a80749ad778fa52d80437c180c90f33e67fb1";
-        sha256 = "sha256-bCkiVizmjVaGOtMu/NYkYblCGu00i5tEjw64b6dEFmk=";
+    owner = "oar-team";
+    repo = "oar3";
+    rev = "2a478a3407246643ad8e192212cb97ff78ac9c28";
+    sha256 = "sha256-th4B35vhfT1jpj5cIF2q3KvqPW4MooQpJoWZHsmoKLA=";
   };
-  
 
   nativeBuildInputs = [ poetry ];
-  
+
   propagatedBuildInputs = with python3Packages; [
-      pyzmq
-      requests
-      alembic
-      procset
-      click
-      simplejson
-      flask
-      tabulate
-      psutil
-      sqlalchemy-utils
-      # simpy
-      # redis
-      # pytest_flask
-      psycopg2
-      # remote_pdb # for debug only
-      passlib
-      escapism
-      toml
-      fastapi
-      uvicorn
+    pyzmq
+    requests
+    alembic
+    procset
+    click
+    simplejson
+    flask
+    tabulate
+    psutil
+    sqlalchemy-utils
+    psycopg2
+    passlib
+    escapism
+    toml
+    fastapi
+    uvicorn
+    pyyaml
+    ptpython
     python-multipart
-   ];
+  ];
 
   doCheck = false;
   #doCheck = true;
@@ -49,7 +45,7 @@ python3Packages.buildPythonPackage rec {
   '';
 
   meta = {
-    broken = true;
+    #broken = true;
     homepage = "https://github.com/oar-team/oar3";
     description = "The OAR Resources and Tasks Management System";
     license = lib.licenses.lgpl3;
