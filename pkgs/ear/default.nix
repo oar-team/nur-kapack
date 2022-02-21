0504f814db2767785da96a21fb3d18acc776e4b7
Olivier Richard <olivier.richard@imag.fr>
2022-02-21 15:01:52 +0100

    ear (wip)

--- pkgs/ear/default.nix
+++ pkgs/ear/default.nix
@@ -2,19 +2,20 @@
 
 stdenv.mkDerivation rec {
   name =  "ear-${version}";
-  version = "4.0.3";
+  version = "4.0.5";
   
-  # src = fetchgit {
-  #   url = "https://gitlab.bsc.es/ear_team/ear.git";
-  #   rev = "d7e206ab289efdbb25a00b408dea748536faeef2";
-  #   sha256 = "sha256-ff/NdZ3jvHGxO4v92+KpzDpHhWcZKVmwuBLs/oZyl5I=";
-  # };
+  src = fetchgit {
+     url = "https://gitlab.bsc.es/ear_team/ear.git";
+     rev = "d7e206ab289efdbb25a00b408dea748536faeef2";
+     sha256 = "sha256-ff/NdZ3jvHGxO4v92+KpzDpHhWcZKVmwuBLs/oZyl5I=";
+  };
 
-  src = /home/auguste/dev/ear;
+  #src = /home/orichard/ear;
+  #src = /home/auguste/dev/ear;
   
   nativeBuildInputs = [ autoreconfHook ];
   
-  buildInputs = [ gsl slurm openmpi ] ++ [(if useMysql then  libmysqlclient else postgresql)] ++ lib.optional cudaSupport cudatoolkit;
+  buildInputs = [ gsl slurm openmpi ] ++ [(if useMysql then libmysqlclient else postgresql)] ++ lib.optional cudaSupport cudatoolkit;
 
   patches = [ ./Makefile.extra.patch ];
 
@@ -31,9 +32,11 @@ stdenv.mkDerivation rec {
 
   configureFlags = [
     "CC=${stdenv.cc}/bin/gcc"
+    "CC_FLAGS=-lm"
+    "DB_DIR=/only_as_build_dep"
     "--with-gsl=${gsl.out}"
     "--with-slurm=slurm"]# "CC_FLAGS=-DSHOW_DEBUGS"]
-    ++ [(if useMysql then "--with-mysql=mysql" else "--with-pgsql=${postgresql.out}")]
+    ++ [(if useMysql then "--with-mysql=${libmysqlclient.out}" else "--with-pgsql=${postgresql.out}")]
     ++ (if useAvx512 then [] else ["--disable-avx512"])
     ++ (if cudaSupport then ["--with-cuda=${cudatoolkit.out}"] else []);
 
