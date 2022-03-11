d0569f3c277cd0457c1553e1f6e27cc1c6d1f6e7
Jonathan Bleuzen <jonathan.bleuzen@inria.fr>
2022-03-11 09:41:41 +0100

    flake - test after Milian review

--- pkgs/melissa/default.nix
+++ pkgs/melissa/default.nix
@@ -1,42 +1,45 @@
-{ stdenv, lib, fetchgit, bash, cmake, pkg-config, gfortran, python37, zeromq, openmpi, libsodium, pandoc, ghc }:
+{ stdenv, lib, fetchFromGitLab, bash, cmake, pkg-config, gfortran, python3, zeromq, openmpi, libsodium, ghc }: # pandoc, ghc }:
 
 stdenv.mkDerivation rec {
-  name =  "melissa-${version}";
+  pname =  "melissa-${version}";
   version = "0.7.1";
 
-  src = fetchgit {
-    url = "https://gitlab.inria.fr/melissa/melissa.git";
+  src = fetchFromGitLab {
+    domain = "gitlab.inria.fr";
+    owner = "melissa";
+    repo = "melissa";
     rev = "e6d09dbb4785e074ed91ad3b46d5a1576aed7b2f";
     sha256 = "sha256-IiJadcplNdy4o1xK9LXvD0rvNrWYJKFgYg8o7KDHlNM";
   };
 
-  buildInputs = [ cmake gfortran python37 openmpi zeromq pkg-config libsodium ];
-  propagatedBuildInputs = [ pandoc ];
+  buildInputs = [ cmake gfortran python3 openmpi zeromq pkg-config libsodium ];
+  #propagatedBuildInputs = [ pandoc ];
 
-  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release ${src}" ];
+  #cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release ${src}" ];
+  cmakeBuildType = "Release";
   enableParallelBuilding = false;
   dontUseCmakeBuildDir = true;
 
   postBuild = ''
     for f in $(find $out/bin/ -type f -executable); do
       wrapProgram "$f" \
-        --prefix PATH ":" ${lib.makeBinPath [ name ]}
-        --prefix LD_LIBRARY_PATH ":" ${lib.makeLibraryPath [ name ]}
+        --prefix PATH ":" ${lib.makeBinPath [ pname ]}
+        --prefix LD_LIBRARY_PATH ":" ${lib.makeLibraryPath [ pname ]}
     done
   '';
 
   postInstall = ''
-    substituteInPlace $out/bin/melissa-launcher --replace "python3" "${python37}/bin/python3"
+    substituteInPlace $out/bin/melissa-launcher --replace "python3" "${python3}/bin/python3"
     substituteInPlace $out/share/melissa/melissa/launcher/job_management.py --replace "/bin/sh" "${bash}/bin/sh"
     substituteInPlace $out/share/melissa/melissa/scheduler/slurm.py --replace "/bin/sh" "${bash}/bin/sh"
     patchShebangs $out
   '';
-  
+
   meta = with lib; {
     homepage = "https://melissa-sa.github.io/";
     description = "Melissa is a file avoiding, adaptive, fault tolerant and elastic framework, to run large scale sensitivity analysis on supercomputers";
     license = licenses.bsd3;
     platforms = ghc.meta.platforms;
-    #platforms = platforms.linux;
+    broken = false;
   };
 }
