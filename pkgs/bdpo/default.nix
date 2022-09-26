672f67e97899a1f6592836280eccd1dd7a443be5
Olivier Richard <olivier.richard@imag.fr>
2022-09-26 19:58:03 +0200

    [bdpo] update file list to rpmextract

--- pkgs/bdpo/default.nix
+++ pkgs/bdpo/default.nix
@@ -1,34 +1,38 @@
 { stdenv, lib, fetchgit, autoPatchelfHook, rpmextract, openmpi }:
 
 stdenv.mkDerivation rec {
-  pname =  "bdpo";
-  version = "4.1.1";
-  #src = "/home/orichard/bdpo";
-  src = fetchgit {
+  pname = "bdpo";
+  version = "4.1.0";
+  #src = /home/auguste/dev/bdpo;
+  src1 = fetchgit {
     url = "git@gricad-gitlab.univ-grenoble-alpes.fr:regale/tools/bdpo";
     rev = "fc2a819b1351473f6bd4144098af461a2e03803c";
     sha256 = "1m716warxqdswp80wn3k0qniccb9csr5y8yk4749cn7dslcaia5c";
   };
-  
+
   nativeBuildInputs = [
     autoPatchelfHook
     rpmextract
   ];
-  
+
   buildInputs = [ openmpi ];
   dontBuild = true;
   dontConfigure = true;
-  
+
   unpackPhase = ''
-  rpmextract ${src}/rpm/bull-dynamic-power-optimizer-2.2-Bull.1.66.2.20220627095402.el8.x86_64.rpm
+    for i in 4.1.0-0 slurm-addons-4.0.1-0 translator-4.1.0-0
+    do
+      rpmextract $src/rpm/powerefficiency-dpo-$i.el8.x86_64.rpm
+    done
   '';
 
   installPhase = ''
-    mkdir -p $out/bin
+    mkdir -p $out
     cp -r usr/bin $out
     cp -r etc $out
-  '';  
-   
+    cp -r usr/lib64/slurm $out  
+  '';
+
   meta = with lib; {
     description = "BDPO";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
