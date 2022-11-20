fd71980b0f2633a2d90d4456e0c17de837cbb831
Olivier Richard <olivier.richard@imag.fr>
2022-11-20 17:50:47 +0100

    [bdpo] fix and update src retrieving w/ builtins.fetchGit

--- pkgs/bdpo-pmpiwrapper/default.nix
+++ pkgs/bdpo-pmpiwrapper/default.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation rec {
   pname =  "bdpo";
   version = "4.1.0";
-  #src = /home/auguste/dev/bdpo;
+
   src = builtins.fetchGit {
     url = "git@gricad-gitlab.univ-grenoble-alpes.fr:regale/tools/bdpo?ref=main";
     rev = "edebce50639d6a01e2bbae813ba1bcdb92f71fb1";
