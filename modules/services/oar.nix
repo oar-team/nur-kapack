e7398ef543bfc8b2761fd71dab9df72405ce6e68
Adrien Faure <adrien.faure@protonmail.com>
2022-11-24 18:06:34 +0100

    oar: change php package for phpfpm

--- modules/services/oar.nix
+++ modules/services/oar.nix
@@ -735,6 +735,7 @@ in
       };
 
       services.phpfpm = lib.mkIf cfg.web.drawgantt.enable {
+        phpPackage = [ pkgs.php74 ];
         pools.oar = {
           user = "oar";
           group = "oar";
