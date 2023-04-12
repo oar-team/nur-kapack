83a646100ce3952cb4b79dd22b55f3536ccdfc89
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-04-12 22:43:31 +0200

    separated package for ring

--- pkgs/enoslib/default.nix
+++ pkgs/enoslib/default.nix
@@ -1,20 +1,5 @@
-{ python3Packages, fetchgit, execo, cmake, libssh2, openssl, zlib, ansible, wirerope }:
+{ python3Packages, fetchgit, execo, cmake, libssh2, openssl, zlib, ansible, ring }:
 let
-  ring = python3Packages.buildPythonPackage rec {
-    name = "ring";
-    version = "0.9.1";
-    src = fetchgit {
-      url = "https://github.com/youknowone/ring";
-      rev = "8e4eb90b13d6480e50c63266e041e491c7c41dfe";
-      sha256 = "sha256-VmNXfntVFlXmvx9OjZ0VuIlHY5CPS3N+MJlL8YkrKcw=";
-    };
-    propagatedBuildInputs = with python3Packages; [
-      attrs
-      wirerope
-    ];
-    doCheck = false;
-  };
-
   ssh-python = python3Packages.buildPythonPackage rec {
     pname = "ssh-python";
     version = "0.10.0";
