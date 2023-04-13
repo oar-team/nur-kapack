f7c5d56e67a42759993346148e58acb485716e22
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-04-13 15:16:01 +0200

    fix bad sha pkgs 22.05

--- nur.nix
+++ nur.nix
@@ -1,7 +1,7 @@
 # If called without explicitly setting the 'pkgs' arg, a pinned nixpkgs version is used by default.
 { pkgs ? import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz";
-    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9iK";
+    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
   }) {}
 , pkgs-2111 ? import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz";
