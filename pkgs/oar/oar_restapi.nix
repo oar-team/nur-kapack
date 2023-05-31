84dbfbaebb4f63abe29b112ca3dc45000f6753e4
Adrien Faure <adrien.faure@protonmail.com>
2023-05-31 14:17:26 +0200

    oar_restapi: add sha256 for pure evaluation mode

--- pkgs/oar/oar_restapi.nix
+++ pkgs/oar/oar_restapi.nix
@@ -2,5 +2,5 @@
 
 oar.overrideAttrs (attrs: rec {
   version = "master";
-  src = fetchTarball "https://github.com/oar-team/oar3/archive/402aaeb032b081aa061997a4158951a340becf77.tar.gz";
+  src = fetchTarball {url="https://github.com/oar-team/oar3/archive/402aaeb032b081aa061997a4158951a340becf77.tar.gz"; sha256 = "sha256-BRqFpc/5yY0xZ3VpbeRkAeAQ+aso9ywX8JJHadrm2kM=";};
 })
