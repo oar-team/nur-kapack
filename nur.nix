99eef97540a9185227a53022a4acdaedf3617a06
Adrien Faure <adrien.faure@protonmail.com>
2023-09-01 14:28:26 +0200

    nur: add local system to pkgs2111

--- nur.nix
+++ nur.nix
@@ -10,7 +10,7 @@
       url = "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz";
       sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
     })
-    { }
+    { localSystem = pkgs.system; }
 , debug ? false
 }:
 
