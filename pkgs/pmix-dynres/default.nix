e0bbb2a4ac697c4affaef23ceac200ef289e420f
Olivier Richard <olivier.richard@imag.fr>
2024-06-24 16:22:38 +0200

    [dynres] update pmix

--- pkgs/pmix-dynres/default.nix
+++ pkgs/pmix-dynres/default.nix
@@ -5,20 +5,21 @@
 stdenv.mkDerivation rec {
   pname = "openpmix";
   version = "5.0.0a1";
-
+  
   # src = fetchFromGitHub {
   #   repo = "openpmix";
   #   owner = "openpmix";
   #   rev = "v${version}";
   #   sha256 = "sha256-79zTZm549VRsqeziCuBT6l4jTJ6D/gZaMAvgHZm7jn4=";
   # };
+ 
   src = fetchFromGitLab {
     domain = "gitlab.inria.fr";
     group = "dynres";
     owner = "dyn-procs";
     repo = pname;
-    rev = "55f943f32c3d2866b7239e852040f0a0d97489b6";
-    sha256 = "sha256-nivy2gBWH8uDBPppYV9Gr/Sj60GEDd1GOdRJwntKwNg=";
+    rev = "d5713fde7f998f863690388b9bc6d38a65076936";
+    sha256 = "sha256-7rJpVEb9UG3tfAEhDXKF2UXRTUtLW2tM2rAZJzDkQgo=";
   };
   
   postPatch = ''
