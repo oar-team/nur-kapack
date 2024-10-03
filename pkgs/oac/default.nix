71a436933f8b2926e48e6dbd7b58a3244c80aefb
Olivier Richard <olivier.richard@imag.fr>
2024-10-03 16:08:13 +0200

    add oac: common autoconf macros for openmpi projects

---
+++ pkgs/oac/default.nix
@@ -0,0 +1,8 @@
+{ fetchFromGitHub }: 
+fetchFromGitHub {
+    owner = "open-mpi";
+    repo = "oac";
+    rev = "add67c2ea77a5bbda9361e741107ea5544e66b1d";
+    sha256 = "sha256-M1drOCmuVd622d0v2awtXWUeyC327hKIIXxIuHR8m3w=";
+  }
+    
